e7794610b1d154726221a16dfde95860f3baf4cf
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-07-30 05:52:46 +0700

    Add css borrow theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,26 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Bankmore+&apos;s frontend' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Bankmore+&apos;s frontend',
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+  /*
+  ** Style
+  */
+  css: [
+    '@/assets/css/bootstrap.min.css',
+    '@/assets/css/style.css',
+    '@/assets/css/font-awesome.min.css',
+    '@/assets/css/fontello.css',
+    '@/assets/css/style.css',
+    '@/assets/css/owl.carousel.css',
+    '@/assets/css/owl.theme.css',
+  ],
   /*
   ** Customize the progress bar color
   */
@@ -24,16 +38,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         })
       }
-    }
-  }
+    },
+  },
 }
-
