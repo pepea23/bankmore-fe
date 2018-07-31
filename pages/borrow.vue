c9f9ce2adf9733e06b1a3429d7b372f4ec36918b
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-07-31 19:37:57 +0700

    Create borrow page

---
+++ pages/borrow.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div v-if="users">
+    <ul v-for="item in users" :key="item.id">
+      <li>{{item.username}}</li>
+    </ul>
+  </div>
+  <div v-else>
+    <li>User Not found</li>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  title: 'borrow',
+  async asyncData({ app, params }) {
+    let users = null
+    try {
+      const result = await app.$axios.$get(
+        'http://www.mocky.io/v2/5b60395d2f0000630046177b',
+      )
+      users = result
+    } catch (error) {
+      console.log('error', error)
+    }
+    return {
+      users,
+    }
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style>
+</style>
+
