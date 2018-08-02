31090d3f8c5fa3719c72777f8e3a9deb723ac08a
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-08-02 18:09:27 +0700

    Change menu to bootstrap navbar

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -1,38 +1,43 @@
 <template>
   <div class="row">
-    <div class="col-xl-2 col-lg-2 col-md-12 col-sm-6 col-6">
-      <div class="logo">
-        IN: {{$mq}}
-        <nuxt-link to="/" exact><img src="images/logo.png" alt="Bankmore+" id="logo-bankmore"><b>Bankmore+</b></nuxt-link>
-      </div>
-    </div>
-    <div class="col-xl-10 col-lg-10 col-md-12 col-sm-12 col-12">
-      <div id="navigation" :class="activeSmallScreen">
-        <div id="menu-button" :class="mobileMenuButton" @click="toggleMobileMenu()">Menu</div>
-        <ul :style="showMobileMenu" :class="statusMobileMenu">
-          <nuxt-link tag="li" to="/" exact>
-            <a class="animsition-link">Home</a>
-          </nuxt-link>
-          <nuxt-link tag="li" to="loan-product">
-             <a class="animsition-link">Loan Product</a>
-          </nuxt-link>
-          <nuxt-link tag="li" to="borrow">
-             <a class="animsition-link">Borrow</a>
-          </nuxt-link>
-          <nuxt-link tag="li" to="invester">
-             <a class="animsition-link">Invester</a>
-          </nuxt-link>
-          <nuxt-link tag="li" to="property-project">
-             <a class="animsition-link">Property / Project</a>
-          </nuxt-link>
-          <nuxt-link tag="li" to="property-for-sale-rent">
-             <a class="animsition-link">Property for Sale & Rent</a>
-          </nuxt-link>
-          <nuxt-link tag="li" to="articles">
-             <a class="animsition-link">Articles</a>
-          </nuxt-link>
-        </ul>
-      </div>
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <div class="logo">
+            <nuxt-link to="/" exact><img src="images/logo.png" alt="Bankmore+" id="logo-bankmore">
+              <b>Bankmore+</b>
+            </nuxt-link>
+          </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navbar">
+          <ul class="navbar-nav ml-auto">
+            <nuxt-link tag="li" class="nav-item" to="/" exact>
+              <a class="nav-link">Home</a>
+            </nuxt-link>
+            <nuxt-link tag="li" class="nav-item" to="loan-product">
+              <a class="nav-link">Loan Product</a>
+            </nuxt-link>
+            <nuxt-link tag="li" class="nav-item" to="borrow">
+              <a class="nav-link">Borrow</a>
+            </nuxt-link>
+            <nuxt-link tag="li" class="nav-item" to="invester">
+              <a class="nav-link">Invester</a>
+            </nuxt-link>
+            <nuxt-link tag="li" class="nav-item" to="property-project">
+              <a class="nav-link">Property / Project</a>
+            </nuxt-link>
+            <nuxt-link tag="li" class="nav-item" to="property-for-sale-rent">
+              <a class="nav-link">Property for Sale & Rent</a>
+            </nuxt-link>
+            <nuxt-link tag="li" class="nav-item" to="articles">
+              <a class="nav-link">Articles</a>
+            </nuxt-link>
+          </ul>
+        </div>
+      </nav>
     </div>
   </div>
 </template>
@@ -40,47 +45,25 @@
 <script>
 export default {
   name: 'Main-Menu',
-  data() {
-    return {
-      isMobileMenuToggle: false,
-    }
-  },
-  computed: {
-    isMobile() {
-      return this.$mq === 'xs' || this.$mq === 'sm'
-    },
-    activeSmallScreen() {
-      return { 'small-screen': this.isMobile }
-    },
-    mobileMenuButton() {
-      return {
-        'menu-opened': this.isMobileMenuToggle,
-      }
-    },
-    showMobileMenu() {
-      return {
-        display: this.isMobile && !this.isMobileMenuToggle ? 'none' : 'block',
-      }
-    },
-    statusMobileMenu() {
-      return { open: this.isMobileMenuToggle }
-    },
-  },
-  methods: {
-    toggleMobileMenu() {
-      this.isMobileMenuToggle = !this.isMobileMenuToggle
-    },
-  },
 }
 </script>
 
 <style scoped>
+#logo-bankmore {
+  height: 50px;
+}
+
 .nuxt-link-exact-active > a,
 .next-link-active > a {
   color: #f51f8a !important;
 }
 
-#logo-bankmore {
-  height: 50px;
+.bg-light {
+  background: #fff !important;
+}
+
+.nav-link {
+  font-family: 'Poppins';
+  font-weight: 500;
 }
 </style>
