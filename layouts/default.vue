d7f9be4ca0685c4b82c77f5fb376a04c3b935757
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-08-02 18:47:58 +0700

    Import footer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,20 @@
   <div>
     <main-header></main-header>
     <nuxt/>
+    <main-footer></main-footer>
   </div>
 </template>
 
 <script>
 const MainHeader = () =>
   import(/* webpackChunkName:'main-header-component' */ '@/components/common/MainHeader')
+const MainFooter = () =>
+  import(/* webpackChunkName:'main-footer-component' */ '@/components/common/MainFooter')
 
 export default {
   components: {
     MainHeader,
+    MainFooter,
   },
 }
 </script>
