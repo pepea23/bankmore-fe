ec84738d4803ff601e76f125d01e9f02e208e234
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-08-02 22:21:54 +0700

    add data in template ApplyLoan

--- components/home/ApplyLoan.vue
+++ components/home/ApplyLoan.vue
@@ -1,9 +1,44 @@
 <template>
-  <div></div>
+  <div class="bg-white section-space80">
+        <div class="container">
+            <div class="row">
+                <div class="offset-xl-2 col-xl-8 offset-lg-2 col-lg-8 offset-md-2 col-md-8 col-sm-12 col-12">
+                    <div class="mb100 text-center section-title">
+                        <!-- section title start-->
+                        <h1>วิธีการยื่นขอสินเชื่อ</h1>
+                    </div>
+                    <!-- /.section title start-->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                    <div class="bg-white pinside40 number-block mb30 outline">
+                        <div class="circle"><span class="number">1</span></div>
+                        <h3>เข้าสู่หน้ากรอกข้อมูล และกรอกรายละเอียดพร้อมแนบภาพเอกสาร</h3>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                    <div class="bg-white pinside40 number-block mb30 outline">
+                        <div class="circle"><span class="number">2</span></div>
+                        <h3>รอการตรวจสอบเอกสาร และเริ่มเข้าสู่การประมูล</h3>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                    <div class="bg-white pinside40 number-block mb30 outline">
+                        <div class="circle"><span class="number">3</span></div>
+                        <h3>ประมูลเสร็จสิ้น รับเงิน</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="offset-xl-2 col-xl-8 offset-lg-2 col-lg-8 offset-md-2 col-md-8 col-sm-12 col-12 text-center mt30"> <a href="#" class="btn btn-default">ลงทะเบียน</a> </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'Apply-Loan',
 }
 </script>
