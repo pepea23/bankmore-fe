6ca8692e31c89197e273d0d6c9aece5c8914e427
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-08-02 22:39:12 +0700

    Add component - "Banner"

---
+++ components/home/Banner.vue
@@ -0,0 +1,56 @@
+
+<template>
+      <div class="slider" id="slider">
+        <!-- slider -->
+        <div class="slider-img"><img src="../../static/images/slider-1.jpg" alt="Borrow - Loan Company Website Template" class="">
+            <div class="container">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                    <div class="slider-captions">
+                        <!-- slider-captions -->
+                        <h1 class="slider-title">Choose the Best Loan. Apply Now!</h1>
+                        <p class="slider-text d-none d-xl-block d-lg-block d-sm-block">The low rate you need for the need you want! Call
+                            <br> (555) 123-4567</p>
+                        <a href="loan-listing-image.html" class="btn btn-default">get started today</a> </div>
+                    <!-- /.slider-captions -->
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="slider-img"><img src="../../static/images/slider-2.jpg" alt="Borrow - Loan Company Website Template" class="">
+                <div class="container">
+                    <div class=" col-xl-6 col-lg-6 col-md-12  col-sm-12 col-12">
+                        <div class="slider-captions">
+                            <!-- slider-captions -->
+                            <h1 class="slider-title">Award winning car loans with Borrow</h1>
+                            <p class="slider-text d-none d-xl-block d-lg-block d-sm-block"> Award winning car loans with low fixed rates and no ongoing fees.</p>
+                            <a href="loan-listing-image.html" class="btn btn-default ">View Products</a> </div>
+                        <!-- /.slider-captions -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="slider-img"><img src="../../static/images/slider-3.jpg" alt="Borrow - Loan Company Website Template" class="">
+                <div class="container">
+                    <div class="col-xl-6 col-lg-6 col-md-12  col-sm-12 col-12">
+                        <div class="slider-captions">
+                            <!-- slider-captions -->
+                            <h1 class="slider-title">Education Loan From Borrow Rate Low Of Interest.</h1>
+                            <p class="slider-text d-none d-xl-block d-lg-block d-sm-block">Education Loan From Avanse At An Attractive Rate Of Interest. Apply Now!</p>
+                            <a href="loan-listing-image.html" class="btn btn-default">View Products</a> </div>
+                        <!-- /.slider-captions -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//<script type="text/javascript" src="js/owl.carousel.min.js">
+//<script type="text/javascript" src="js/slider-carousel.js">
+//<script type="text/javascript" src="js/service-carousel.js">
+export default {
+  name: 'Banner',
+}
+</script>
