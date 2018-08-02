8bb90d7c290bd3310196f3ef766376e49ed1efca
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-08-02 21:47:28 +0700

    Add component - "User Review"

---
+++ components/home/UserReview.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="bg-white section-space80">
+        <div class="container">
+            <div class="row">
+                <div class="offset-xl-2 col-xl-8 offset-lg-2 col-lg-8 offset-md-2 col-md-8 col-sm-12 col-12">
+                    <div class="mb60 text-center section-title">
+                        <!-- section title start-->
+                        <h1>คำชมจาก</h1>
+                        <p>Reviews จากผู้ชำนาญการด้านการเงิน</p>
+                    </div>
+                    <!-- /.section title start-->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-4 col-sm-12 clearfix col-12">
+                    <div class="testimonial-block mb30 text-center">
+                        <div class=" mb20 testimonial-img-1"> <img src="images/testimonial-img.jpg" alt="Borrow - Loan Company Website Template" class="img-circle"> </div>
+                        <div class="mb20">
+                            <p class="testimonial-text"> “I loved the customer service you guys provided me. That was very nice and patient with questions I had. I would really like definitely come back here”</p>
+                        </div>
+                        <div class="testimonial-autor-box">
+                            <div class="testimonial-autor">
+                                <h4 class="testimonial-name-1">Donny J. Griffin</h4>
+                                <span class="testimonial-meta">Personal Loan</span> </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-4 col-sm-12 clearfix col-12">
+                    <div class="testimonial-block mb30 text-center">
+                        <div class="mb20 testimonial-img-1"> <img src="images/testimonial-img-1.jpg" alt="Borrow - Loan Company Website Template" class="img-circle"> </div>
+                        <div class="mb20">
+                            <p class="testimonial-text">Donec arcu lacus, accumsan vel metus laoreet, pulvinar imperdiet nisi lorem sipusmn ras vel sapien imperdiet nisi sagittis ornare sed congue sem.</p>
+                        </div>
+                        <div class="testimonial-autor-box">
+                            <div class="testimonial-autor">
+                                <h4 class="testimonial-name-1">Mary O. Randle</h4>
+                                <span class="testimonial-meta">Education Loan</span> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'User-Review',
+}
+</script>
