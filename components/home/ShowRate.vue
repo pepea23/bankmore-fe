aade1b30d936f7cb274b5881facc33d711df4ca1
Rapepat Pengsart <pluam2541@hotmail.co.th>
2018-08-03 15:06:50 +0700

    add [components : ShowRate]

---
+++ components/home/ShowRate.vue
@@ -0,0 +1,47 @@
+<template>
+<body>
+    
+<div class="rate-table">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                    <div class="rate-counter-block">
+                        <div class="icon rate-icon  "> <img src="images/mortgage.svg" alt="Borrow - Loan Company Website Template" class="icon-svg-1x"></div>
+                        <div class="rate-box">
+                            <h1 class="loan-rate">3.74%</h1>
+                            <small class="rate-title">ดอกเบี้ยสินเชื่อเริ่มต้นต่อปี</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                    <div class="rate-counter-block">
+                        <div class="icon rate-icon  "> <img src="images/loan.svg" alt="Borrow - Loan Company Website Template" class="icon-svg-1x"></div>
+                        <div class="rate-box">
+                            <h1 class="loan-rate">8.96%</h1>
+                            <small class="rate-title">ดอกเบี้ย MLR</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                    <div class="rate-counter-block">
+                        <div class="icon rate-icon  "> <img src="images/car.svg" alt="Borrow - Loan Company Website Template" class="icon-svg-1x"></div>
+                        <div class="rate-box">
+                            <h1 class="loan-rate">6.70%</h1>
+                            <small class="rate-title">ดอกเบี้ย MRR</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+    </template>
+
+<script>
+export default {
+  name: 'ShowRate',
+}
+</script>
+
+
+    
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,20 @@
 <template>
 <div>
-  <div v-if="users">
-    <ul v-for="item in users" :key="item.id">
-      <li>{{item.username}}</li>
-    </ul>
-  </div>
-  <div v-else>
-    <li>User Not found</li>
-  </div>
+<ShowRate></ShowRate>
 </div>
 </template>
 
 <script>
+
+const ShowRate = () => import('@/components/home/ShowRate')
 export default {
   title: 'หน้าแรก',
-  async asyncData({ app, params }) {
-    let users = null
-    try {
-      const result = await app.$axios.$get(
-        'http://www.mocky.io/v2/5b60395d2f0000630046177b',
-      )
-      users = result
-    } catch (error) {
-      console.log('error', error)
-    }
-    return {
-      users,
-    }
-  },
   data() {
     return {}
   },
+  components: { 
+   ShowRate,
+  }, 
 }
 </script>
 
