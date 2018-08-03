f0876114094b1c03efd355b84b8ab165d97cda63
Rapepat Pengsart <pluam2541@hotmail.co.th>
2018-08-03 15:37:04 +0700

    modify [components HowtoEntry]

--- components/borrow/HowToEntry.vue
+++ components/borrow/HowToEntry.vue
@@ -1,44 +1,27 @@
 <template>
-  <div class="bg-white section-space80">
-        <div class="container">
+<div class="container">
             <div class="row">
-                <div class="offset-xl-2 col-xl-8 offset-lg-2 col-lg-8 offset-md-2 col-md-8 col-sm-12 col-12">
-                    <div class="mb100 text-center section-title">
-                        <h1>วิธีการกรอกข้อมูล</h1>
+                <div class="col-xl-12 col-md-12 col-lg-12 col-sm-12 col-12">
+                    <div class="wrapper-content bg-white">
+                        <div class="section-scroll pinside60" id="section-about">
+                            <h1>วิธีกรอกข้อมูล</h1>
+                            <p class="lead">การกรอกข้อมูลผู้ขายฝาก</p>
+                            <div class="row">
+                                 <div class="col-xl-6 col-md-6 col-lg-6 col-sm-12 col-12">
+                                    <p>กรณีบุคคลธรรมดา: ชื่อ-นามสกุล, ที่อยู่, เบอร์ติดต่อ, Email, บุคคลอ้างอิง, เกิดวันที่, เกิดวันที่, อายุ, สถานภาพสมรส, จำนวนบุตร, การศึกษา</p>
+                                </div>
+                                <div class="col-xl-6 col-md-6 col-lg-6 col-sm-12 col-12">
+                                    <p>กรณีนิติบุคคล/เจ้าของกิจการ: ชื่อกิจการ, เลขที่จดทะเบียน, วันที่จดทะเบียน, จำนวนกรรมการบริษัท, เงื่อนไขการลงนาม</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <p>โปรดตรวจสอบการกรอกข้อมูลและอ่านสัญญาข้อตกลงให้ครบถ้วนถูกต้อง</p>
+                            <a href="#" class="btn btn-default">Apply for loan</a> 
+                        </div>           
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
-                    <div class="bg-white pinside40 number-block mb30 outline">
-                        <div class="circle"><span class="number">1</span></div>
-                        <h3>เข้าสู่หน้ากรอกข้อมูล</h3>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
-                    <div class="bg-white pinside40 number-block mb30 outline">
-                        <div class="circle"><span class="number">2</span></div>
-                        <h3>กรอกรายระเอียดข้อมูล</h3>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
-                    <div class="bg-white pinside40 number-block mb30 outline">
-                        <div class="circle"><span class="number">3</span></div>
-                        <h3>ตรวจสอบข้อมูลที่กรอกว่าครบถ่วนถูกต้อง</h3>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
-                    <div class="bg-white pinside40 number-block mb30 outline">
-                        <div class="circle"><span class="number">4</span></div>
-                        <h3>กดลงทะเบียน</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="offset-xl-2 col-xl-8 offset-lg-2 col-lg-8 offset-md-2 col-md-8 col-sm-12 col-12 text-center mt30"> <a href="#" class="btn btn-default">สมัครสมาชิค</a> </div>
             </div>
         </div>
-    </div>
 </template>
 
 <script>
