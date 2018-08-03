0f15f9e9f6368d9141dc016b6f5bc95d568ce945
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-08-04 01:11:40 +0700

    Create AuctionItem

---
+++ components/widget/AuctionItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+    <div class="service-img-box mb30 text-center">
+      <div class="service-img">
+        <nuxt-link :to="link" class="imghover"><img :src="image" :alt="title" class="img-fluid"></nuxt-link>
+      </div>
+      <div class="service-content bg-white pinside30 outline">
+        <h2>
+          <nuxt-link :to="link" class="title">{{title}}</nuxt-link>
+        </h2>
+        <p>{{description}}</p>
+        <nuxt-link :to="link" class="btn-link">{{buttonText}}</nuxt-link>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Auction-Item',
+  props: {
+    title: String,
+    description: String,
+    image: String,
+    link: String,
+    buttonText: String,
+  },
+}
+</script>
