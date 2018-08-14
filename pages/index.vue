f1269079bb47bc5e935efdf58ce3d7a1cfd27436
Nattapon Pondongnok <nainatjab999@gmail.com>
2018-08-14 11:04:40 +0700

    Import Auction List

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
         <div class="wrapper-content">
           <div class="row">
-            <AuctionItem v-for="item in items" :key="item.id" :title="item.title" :image="item.image" :description="item.description" :link="item.link" buttonText="Show More" />
+            <auction-list :items="items"></auction-list>
             <banner></banner>
             <apply-loan></apply-loan>
             <choose-us></choose-us>
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-const AuctionItem = () => import('@/components/widget/AuctionItem')
+const AuctionList = () => import('@/components/widget/AuctionList')
 const ShowRate = () => import('@/components/home/ShowRate')
 const Banner = () => import('@/components/home/Banner')
 const ApplyLoan = () => import('@/components/home/ApplyLoan')
@@ -27,45 +27,42 @@ const UserReview = () => import('@/components/home/UserReview')
 export default {
   title: 'หน้าแรก',
   components: {
-    AuctionItem,
-  },
-  data() {
-    return {
-      items: [
-        {
-          id: 1,
-          title: 'Test Auction',
-          image: 'images/blog-img.jpg',
-          description: 'Test description',
-          link: 'auction/1',
-        },
-        {
-          id: 2,
-          title: 'Test Auction 2',
-          image: 'images/blog-img.jpg',
-          description: 'Test description 2',
-          link: 'auction/2',
-        },
-        {
-          id: 3,
-          title: 'Test Auction 3',
-          image: 'images/blog-img.jpg',
-          description: 'Test description 3',
-          link: 'auction/3',
-        },
-      ],
-    }
-  },
-  data() {
-    return {}
-  },
-  components: {
+    AuctionList,
     ShowRate,
     Banner,
     ApplyLoan,
     ChooseUs,
     UserReview,
   },
+  data() {
+    return {
+      items :
+          [
+            {
+              id: 1,
+              title: 'Test Auction',
+              image: 'images/blog-img.jpg',
+              description: 'Test description',
+              link: 'auction/1',
+            },
+            {
+              id: 2,
+              title: 'Test Auction 2',
+              image: 'images/blog-img.jpg',
+              description: 'Test description 2',
+              link: 'auction/2',
+            },
+            {
+              id: 3,
+              title: 'Test Auction 3',
+              image: 'images/blog-img.jpg',
+              description: 'Test description 3',
+              link: 'auction/3',
+            },
+          ]
+    }
+  },
+
 }
 </script>
 
