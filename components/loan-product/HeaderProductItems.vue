8b2758fbf6ccdbfcd8ebd3ed5539746b36d12b68
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-08-15 21:17:07 +0700

    Add component - "Header Product Items"

---
+++ components/loan-product/HeaderProductItems.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="page-header">
+        <div class="container">
+            <div class="row">
+               <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="page-breadcrumb">
+                        <ol class="breadcrumb">
+                            <li><a href="index.html">Home</a></li>
+                            <li class="active">Borrow Credit Card</li>
+                        </ol>
+                    </div>
+                </div>
+               <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="bg-white pinside30">
+                        <div class="row">
+                           <div class="col-xl-4 col-lg-4 col-md-9 col-sm-12 col-12">
+                                <h1 class="page-title">Credit Card Listing</h1>
+                            </div>
+                            <div class="col-xl-8 col-lg-8 col-md-3 col-sm-12 col-12">
+                                <div class="row">
+                                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <div class="btn-action"> <a href="#" class="btn btn-default">How To Apply</a> </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="sub-nav-sticky-wrapper" class="sticky-wrapper" style="height: 58px;"><div class="sub-nav" id="sub-nav" style="width: 998px;"><div id="menu-button">subnav</div>
+                         <ul class="nav nav-justified">
+                            <li class="nav-item">
+                                <a href="contact-us.html" class="nav-link">Give me call back</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Emi Caculator</a>
+                            </li>
+                        </ul>
+                    </div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header-Product-Items',
+}
+</script>
