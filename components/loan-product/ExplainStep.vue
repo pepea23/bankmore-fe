10a74eba8364c744e006c3167a2646440e1176cb
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-08-15 22:35:08 +0700

    Add component "ผลิตภัณฑ์ทางการเงิน"

---
+++ components/loan-product/ExplainStep.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="section-scroll" id="section-typeloan">
+    <div class="bg-light pinside60">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="section-title mb60">
+                    <h2>Get financing for whatever you need now</h2>
+                    <p>Achieve all your goals and aspirations; with the right kind of help, exactly when you need it.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-blurb mb60">
+                    <h3>New Car Loan</h3>
+                    <p>Borrow-Loan Company offers new car loan at fair interest rates and adjustable repayment terms.Fast sanction, personalised service and absolute transparency in new car loans.</p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-blurb mb60">
+                    <h3>Pre Owend Car</h3>
+                    <p>We have tie-ups across the widest range of authorised pre-owned car dealers. Attractive car loans for used cars. Pre-Owned car finance available at Borrow Loan Company.</p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-blurb mb60">
+                    <h3>Loan Against Car</h3>
+                    <p>With no income documents required, getting a loan couldn’t get any easier. Borrow-Loan Company offers you car loan at basic interest rates and flexible EMI for repayment.</p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-blurb mb60">
+                    <h3>Other Loan</h3>
+                    <p>Borrow-Loan Company also grant different loans like Home loan, Personal loan and Educate loan at fair rate of interest and flexible EMI. For more detail you can check our loan products.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Explain-Step',
+}
+</script>
---
+++ components/loan-product/Features.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="section-scroll" id="section-feature">
+    <div class="pinside60">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="section-title mb60">
+                    <h2>Features of Car Loan</h2>
+                    <p> All loans are not created equal, personal loan has become a great option for people to use.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-icon mb30">
+                    <div class="icon mb20"><i class="icon-dollar-bill icon-default icon-2x"></i></div>
+                    <h3>Cash Loan Facility</h3>
+                    <p>Maecenas in ultricies sem. Nunc eget orci mi. Sed porttitor s, tellus fringilla condimentum eglis elit dictum cerat. </p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-icon mb30">
+                    <div class="icon mb20"><i class="icon-documentation icon-default icon-2x"></i></div>
+                    <h3>Special Preapproved Loan</h3>
+                    <p>Pellentesque mollis, diam a viverra luctus, nisl dui vehicula erat, id congue ante hicula tellus sit amet.</p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-icon mb30">
+                    <div class="icon mb20"><i class="icon-money icon-default icon-2x"></i></div>
+                    <h3>No Varifactions</h3>
+                    <p>Our loan rates and charges are very attractive lorem ipsums sitamet uerse ipsum.Curabitulectus mattis vitae. </p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="feature-icon mb30">
+                    <div class="icon mb20"><i class="icon-dollar-coins icon-default icon-2x"></i></div>
+                    <h3>150% Funding on Loan</h3>
+                    <p>Nunc eget orci mi. Sed porttitor lacus quis scelerisque dignissim. Nullam bibendu msfeus, isapien dolor et dui.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'Features',
+}
+</script>
