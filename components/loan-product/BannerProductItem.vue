38093c85d4d87c2f9f792bbc482075612b7f7bf8
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-08-18 19:59:08 +0700

    Rename BannerProduct(s)

--- components/loan-product/BannerProductItem.vue
+++ components/loan-product/BannerProductItem.vue
@@ -2,19 +2,19 @@
     <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
         <div class="card-listing">
             <div class="card-img">
-                <nuxt-link :to="linkDetail"><img :src="image" :alt="title"></nuxt-link>
+                <nuxt-link :to=" 'loan-product/'+id"><img :src="image" :alt="title"></nuxt-link>
             </div>
             <div class="card-content">
                 <h3 class="card-name">
-                    <nuxt-link :to="linkDetail" class="title">{{title}}</nuxt-link>
+                    <nuxt-link :to=" 'loan-product/'+id" class="title">{{title}}</nuxt-link>
                 </h3>
                 <div class="card-features ">
                     <ul class="listnone">
                         <li>{{description}}</li>
                     </ul>
                 </div>
-                <nuxt-link :to="link" class="btn btn-default btn-sm">Apply Now</nuxt-link>
-                <nuxt-link :to="linkDetail" class="btn-link">Read More</nuxt-link>
+                <nuxt-link :to="'apply-product/'+id" class="btn btn-default btn-sm">Apply Now</nuxt-link>
+                <nuxt-link :to=" 'loan-product/'+id" class="btn-link">Read More</nuxt-link>
             </div>
         </div>
     </div>
@@ -28,8 +28,7 @@ export default {
     image: String,
     title: String,
     description: String,
-    link: String,
-    linkDetail: String,
+    id: String,
   },
 }
 </script>
