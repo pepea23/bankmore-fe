7884439f9546b64d36c156de100ece11d13ff1dc
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-09-12 23:35:15 +0700

    [Edit property product] สร้างให้รับค่าผ่าน props

--- components/Cards/CardProduct.vue
+++ components/Cards/CardProduct.vue
@@ -1,106 +1,39 @@
 <template>
-    <div class="row">
-      <div v-for="product in product" :key="product">
-        <div class="col-xl-4 col-md-4 col-6">
-              <div class="card">
-                <div class="card-img">
-                  <img :src="product.image" :alt="title" />
-                </div>
-                <div class="card-content">
-                  <h3 class="card-name">
-                    {{product.title}}
-                  </h3>
-                  <div class="card-features ">
-                    <ul class="listnone">
-                      <li>{{product.description}}</li>
-                    </ul>
-                  </div>
-                  <div class="text-danger fontCountdown d-flex justify-content-center">
-                  <Countdown deadline="August 22, 2022"></Countdown>
-									</div>
-                </div>
-              </div>
+<div class="row">
+  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" v-for="product in product" :key="product">
+    <div class="card">
+          <img :src="product.image" :alt="title" />
+          <div class="card-content">
+            <center><h3 class="card-name">{{product.title}}</h3></center>
+            <div class="card-features ">
+              <ul class="listnone">
+                <center><li>{{product.description}}</li></center>
+              </ul>
+            </div>
+            <div class="text-danger fontCountdown d-flex justify-content-center">
+              <!-- <Countdown deadline="August 22, 2022"></Countdown> -->
+              <Countdown end="14-Sep-2018 23:00:00"></Countdown>
+            </div>
           </div>
-        </div>
-			</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import Countdown from 'vuejs-countdown'
+import Countdown from 'vuejs-countdown'
 
-  export default {
-    title: 'property-project',
-    components: {
-      Countdown
-    },
-    data() {
-      return {
-        product: [
-          {
-            id: 1,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty1',
-            description: 'Proporty detail 1',
-          },
-          {
-            id: 2,
-            image: 'images/blog-img-2.jpg',
-            title: 'Proporty2',
-            description: 'Proporty detail 2',
-          },
-          {
-            id: 3,
-            image: 'images/blog-img-3.jpg',
-            title: 'Proporty3',
-            description: 'Proporty detail 3',
-          },
-          {
-            id: 4,
-            image: 'images/blog-img-4.jpg',
-            title: 'Proporty4',
-            description: 'Proporty detail 4',
-          },
-          {
-            id: 5,
-            image: 'images/blog-img-5.jpg',
-            title: 'Proporty5',
-            description: 'Proporty detail 5',
-          },
-          {
-            id: 6,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty6',
-            description: 'Proporty detail 6',
-          }, {
-            id: 7,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty7',
-            description: 'Proporty detail 7',
-          }, {
-            id: 8,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty8',
-            description: 'Proporty detail 8',
-          },
-        ],
-      }
-    },
-    methods: {
-
-    }
-  }
-
+export default {
+  title: 'property-project',
+  components: {
+    Countdown,
+  },
+  props: {
+    product: Array,
+  },
+  methods: {},
+}
 </script>
 
 <style>
-  .paddingAll {
-    padding: 5%;
-  }
-
-  .fontCountdown {
-    font-size:5%;
-  }
-	.card{
-		width:350px;;
-	}
 </style>
--- pages/property-project.vue
+++ pages/property-project.vue
@@ -1,87 +1,82 @@
 <template>
-  <div class="container">
-    <div class="row ">
-    <div class="col-12">
-    <div class="row">
-      <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
-        <CardSearch/>
-      </div>
-      <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
-        <CardProduct/>
-      </div>
-      </div>
-      </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 col-12">
+      <CardSearch />
+    </div>
+    <div class="col-md-8 col-12">
+      <CardProduct :product="product"></CardProduct>
     </div>
   </div>
+</div>
 </template>
 
 <script>
-  import Countdown from 'vuejs-countdown'
-  const CardSearch = () => import('@/components/Cards/CardSearch')
-  const CardProduct = () =>import('@/components/Cards/CardProduct')
+import Countdown from 'vuejs-countdown'
+const CardSearch = () => import('@/components/Cards/CardSearch')
+const CardProduct = () => import('@/components/Cards/CardProduct')
 
-  export default {
-    title: 'property-project',
-    components: {
-      CardSearch,
-      Countdown,
-      CardProduct
-    },
-    data() {
-      return {
-        product: [
-          {
-            id: 1,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty1',
-            description: 'Proporty detail 1',
-          },
-          {
-            id: 2,
-            image: 'images/blog-img-2.jpg',
-            title: 'Proporty2',
-            description: 'Proporty detail 2',
-          },
-          {
-            id: 3,
-            image: 'images/blog-img-3.jpg',
-            title: 'Proporty3',
-            description: 'Proporty detail 3',
-          },
-          {
-            id: 4,
-            image: 'images/blog-img-4.jpg',
-            title: 'Proporty4',
-            description: 'Proporty detail 4',
-          },
-          {
-            id: 5,
-            image: 'images/blog-img-5.jpg',
-            title: 'Proporty5',
-            description: 'Proporty detail 5',
-          },
-          {
-            id: 6,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty6',
-            description: 'Proporty detail 6',
-          }, {
-            id: 7,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty7',
-            description: 'Proporty detail 7',
-          }, {
-            id: 8,
-            image: 'images/blog-img-1.jpg',
-            title: 'Proporty8',
-            description: 'Proporty detail 8',
-          },
-        ],
-      }
-    },
-    methods: {
-
+export default {
+  title: 'property-project',
+  components: {
+    CardSearch,
+    Countdown,
+    CardProduct,
+  },
+  data() {
+    return {
+      product: [
+        {
+          id: 1,
+          image: 'images/blog-img-1.jpg',
+          title: 'Proporty1',
+          description: 'Proporty detail 1',
+        },
+        {
+          id: 2,
+          image: 'images/blog-img-2.jpg',
+          title: 'Proporty2',
+          description: 'Proporty detail 2',
+        },
+        {
+          id: 3,
+          image: 'images/blog-img-3.jpg',
+          title: 'Proporty3',
+          description: 'Proporty detail 3',
+        },
+        {
+          id: 4,
+          image: 'images/blog-img-4.jpg',
+          title: 'Proporty4',
+          description: 'Proporty detail 4',
+        },
+        {
+          id: 5,
+          image: 'images/blog-img-5.jpg',
+          title: 'Proporty5',
+          description: 'Proporty detail 5',
+        },
+        {
+          id: 6,
+          image: 'images/blog-img-1.jpg',
+          title: 'Proporty6',
+          description: 'Proporty detail 6',
+        },
+        {
+          id: 7,
+          image: 'images/blog-img-1.jpg',
+          title: 'Proporty7',
+          description: 'Proporty detail 7',
+        },
+        {
+          id: 8,
+          image: 'images/blog-img-1.jpg',
+          title: 'Proporty8',
+          description: 'Proporty detail 8',
+        },
+      ],
     }
-  }
-
+  },
+  methods: {},
+}
 </script>
